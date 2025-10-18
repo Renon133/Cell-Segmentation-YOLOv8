610dc43ab2afc1ff448944d56e940530960ceaf1
Balkrishan Sehra <leon@Balkrishans-MacBook-Pro-3.local>
2025-10-18 20:05:39 +0530

    Dependecies/setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cell_segmentation",
+    version="0.1.0",
+    author="Balkrishna Sehra",
+    author_email="balkrishan.sehra03@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
