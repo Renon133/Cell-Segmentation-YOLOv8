8e74f7df7a12374be5260738ff1bbd301b431a9a
Balkrishan Sehra <leon@Balkrishans-MacBook-Pro-3.local>
2025-10-18 20:31:22 +0530

    Logger added

---
+++ app.py
@@ -0,0 +1,3 @@
+from cell_segmentation.logger import logging
+
+logging.info("Starting the cell segmentation application...")
---
+++ cell_segmentation/logger/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+from from_root import from_root
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# create logs directory and construct log file path
+log_dir_path = os.path.join(from_root(), "logs", LOG_FILE)
+os.makedirs(log_dir_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_dir_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s: %(levelname)s: %(name)s]: %(message)s",
+    level=logging.INFO,
+)
